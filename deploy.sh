c67e9ccad2b35793616c266812b73653a959cb49
Jacob Adams <jacob@tookmund.com>
2019-08-01 10:03:19 +0100

    deploy: Ensure all changes are committed and pushed

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 DEPLOY="public_html/acm"
+
+# Preferably separate commits but this ensures changes are pushed
+git commit -a
+git pull
+git push
+
 jekyll build
 rsync -rcvz --delete _site/ "bg3.cs.wm.edu:$DEPLOY/"
