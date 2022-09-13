4bda80229dc829ceb1dced043525707bb12fa19f
Joseph Lee <jslee08@wm.edu>
2022-09-13 13:21:18 -0400

    Separate deploy scripts for debugging/covenience

---
+++ deploy_joseph_lee.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+DEPLOY="/home/acm"
+
+# Preferably separate commits but this ensures changes are pushed
+git commit -a
+git pull
+git push
+
+jekyll build --trace
+rsync -rcvz --delete --exclude='.git*' --exclude='.zfs' _site "joegen@th121-21.cs.wm.edu:$DEPLOY/"
